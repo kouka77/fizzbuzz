89beda0e16cbeab1ce8c51fdfbdd3803f6fac76b
aymen-mansour <aymen.mansour@hotmail.com>
2018-01-19 23:02:45 +0100

    aymen

---
+++ job.py
@@ -0,0 +1,25 @@
+"""Fizzbuzz Program"""
+
+import threading
+DEBUT = 1
+FIN = 100
+
+def fizzbuzz(nbr):
+    """Print job"""
+    if nbr % 3 == 0:
+        if nbr % 5 == 0:
+            print("FizzBuzz")
+        else:
+            print("Fizz")
+    elif nbr % 5 == 0:
+        print("Buzz")
+    else:
+        print(nbr)
+# append threads
+threads = [threading.Thread(target=fizzbuzz, args=(i,)) for i in range(DEBUT, FIN+1)]
+# Start all threads
+for t in threads:
+    t.start()
+# Ensure all of the threads have finished
+for t in threads:
+    t.join()
